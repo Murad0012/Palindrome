4b34c8d265cf585da1b533252ee0e53363fcd679
Predator <murad.mammedzade11@gmail.com>
2023-08-07 01:30:27 -0700

    First change

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.Tracing;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string word = "radar";
+        int i = 0;
+        int j = word.Length - 1;
+        for (; i < j;)
+        {
+            if (word[i] == word[j])
+            {
+                i++;
+                j--;
+            }
+            else
+            {
+                break;
+            }
+        }
+        if (i > j || i == j)
+        {
+            Console.WriteLine("Is a palindrome");
+        }
+        else
+        {
+            Console.WriteLine("Is not a palindrome");
+        }
+    }
+}
